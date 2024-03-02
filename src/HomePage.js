86998501dee1ea8858c3de7644fcd6b29c9fa8d0
msurve2@uw.edu <msurve2@uw.edu>
2024-03-01 16:02:18 -0800

    added home page buttons

--- src/HomePage.js
+++ src/HomePage.js
@@ -11,6 +11,35 @@ function HomePage() {
         <h1>Welcome to UW Study Buddy</h1>
         <img src={studyBuddyLogo} alt="Study Buddy Logo" />
         <h4>Unlock Your Learning Legacy: Connect, Sell, Succeed!</h4>
+
+        <div className="home-btns">
+                <ul>
+                    <li>
+                        <button onClick={() => this.navigateToPage('profile.html')}>
+                            <img src="profile_logo.png" alt="Profile Logo" />
+                            <span>Profile</span>
+                        </button>
+                    </li>
+                    <li>
+                        <button onClick={() => this.navigateToPage('shop.html')}>
+                            <img src="shop_logo.png" alt="Shop Logo" />
+                            <span>Shop</span>
+                        </button>
+                    </li>
+                    <li>
+                        <button onClick={() => this.navigateToPage('sell.html')}>
+                            <img src="sell_logo.png" alt="Sell Logo" />
+                            <span>Sell</span>
+                        </button>
+                    </li>
+                    <li>
+                        <button onClick={() => this.navigateToPage('friends.html')}>
+                            <img src="friends_logo.png" alt="Friends Logo" />
+                            <span>Friends</span>
+                        </button>
+                    </li>
+                </ul>
+            </div>
       </main>
       <Footer />
     </div>

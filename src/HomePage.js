8f3f446157880e730f6a7b5b44d73eda4fd33353
devanshidesai <ddesai22@uw.edu>
2024-03-01 16:55:21 -0800

    added links for home button

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import studyBuddyLogo from './img/studybuddy-logo.png';
@@ -8,42 +9,43 @@ import sellLogo from './img/sell.png';
 import friendsLogo from './img/friends.png';
 
 function HomePage() {
+
   return (
     <div>
-    <Header />
+      <Header />
       <main>
         <h1>Welcome to UW Study Buddy</h1>
         <img src={studyBuddyLogo} alt="Study Buddy Logo" />
         <h4>Unlock Your Learning Legacy: Connect, Sell, Succeed!</h4>
 
         <div className="home-btns">
-                <ul>
-                    <li>
-                        <button onClick={() => this.navigateToPage('profile.html')}>
-                            <img src={profileLogo} alt="Profile Logo" />
-                            <span>Profile</span>
-                        </button>
-                    </li>
-                    <li>
-                        <button onClick={() => this.navigateToPage('shop.html')}>
-                            <img src={shopLogo} alt="Shop Logo" />
-                            <span>Shop</span>
-                        </button>
-                    </li>
-                    <li>
-                        <button onClick={() => this.navigateToPage('sell.html')}>
-                            <img src={sellLogo} alt="Sell Logo" />
-                            <span>Sell</span>
-                        </button>
-                    </li>
-                    <li>
-                        <button onClick={() => this.navigateToPage('friends.html')}>
-                            <img src={friendsLogo} alt="Friends Logo" />
-                            <span>Friends</span>
-                        </button>
-                    </li>
-                </ul>
-            </div>
+          <ul>
+            <li>
+              <Link to="/profile">
+                <img src={profileLogo} alt="Profile Logo" />
+                <span>Profile</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/availablelistings">
+                <img src={shopLogo} alt="Shop Logo" />
+                <span>Shop</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/sell">
+                <img src={sellLogo} alt="Sell Logo" />
+                <span>Sell</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/friends">
+                <img src={friendsLogo} alt="Friends Logo" />
+                <span>Friends</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </main>
       <Footer />
     </div>

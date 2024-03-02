7d08294b8a5d7c7badc409e5348074bf9251b2eb
msurve2@uw.edu <msurve2@uw.edu>
2024-03-01 17:02:43 -0800

    homepage header

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,14 @@
 // Header.js
 import React from 'react';
 import {Link} from 'react-router-dom';
+import studyBuddyLogo from './img/studybuddy-logo.png';
 
 
 function Header() {
   return (
     <div class="menu-bar">
         <ul>
+            <li><img src={studyBuddyLogo} alt="Study Buddy Logo" /></li>
             <li><a href="/">Home</a></li>
             <li><a href="Profile">Profile</a></li>
             <li><a href="AvailableListings">Shop</a></li>
